834d5b04128df512ec7a5f2bfd953eb9aca57074
podkowa01 <nicole.podkowa@escola.pr.gov.br>
2026-04-09 11:10:44 -0300

    .

---
+++ main.js
@@ -0,0 +1,30 @@
+/* código escondido */
+
+@media screen and (min-width: 768px){
+    .botoes{
+        display:flex;
+    }
+} 
+/* código escondido */
+
+@media screen and (min-width: 768px){
+    .botoes{
+        display:flex;
+    }
+    .botao:first-child {
+    border-radius: 40px 0 0 0;
+    }
+}
+/* código escondido */
+
+@media screen and (min-width: 768px){
+    .botoes{
+        display:flex;
+    }
+    .botao:first-child {
+        border-radius: 40px 0 0 0;
+    }
+    .botao:last-child {
+        border-radius: 0 40px 0 0;
+    }
+}
